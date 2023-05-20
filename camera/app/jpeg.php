299da94cf291b23f19ceefc821d69bf8b829c914
Yann Challet <yann@cymdev.com>
2023-05-20 09:19:38 +0200

    Img Rotate

--- camera/app/jpeg.php
+++ camera/app/jpeg.php
@@ -9,10 +9,16 @@ class jpeg
 {
     public static function postProccess(String $path)
     {
-        $GD = \imagecreatefromjpeg(Storage::path($path));
+        $GD = imagecreatefromjpeg(Storage::path($path));
+        $postProccess = false;
         if (parameters::get('jpeg rotate', 0) !== 0) {
             $GD = self::rotate($GD, parameters::get('jpeg rotate', 0));
+            $postProccess = true;
         }
+        if ($postProccess) {
+            imagejpeg($GD, Storage::path($path), parameters::get('jpeg quality', 97));
+        }
+        
     }
 
     public static function rotate($GD, $degrees)
